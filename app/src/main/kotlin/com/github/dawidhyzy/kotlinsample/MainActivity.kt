116e35e794c5b40eba912776bce5fa33adaf825d
Dawid Hyży <dawidhyzy@gmail.com>
2016-02-12 08:22:20 +0100

    working project

--- app/src/main/kotlin/com/github/dawidhyzy/kotlinsample/MainActivity.kt
+++ app/src/main/kotlin/com/github/dawidhyzy/kotlinsample/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.github.dawidhyzy.kotlinsample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
     }
 }
